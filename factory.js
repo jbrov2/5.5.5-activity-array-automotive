f7baf4eb4eb6b99651ce8b43799b89fbb50079ae
zephyr <jafaribrownsha@gmail.com>
2023-09-20 22:02:02 -0400

    added the factory constructior, and necessary func

--- factory.js
+++ factory.js
@@ -1,3 +1,22 @@
 class Factory{
-    
+    constructor(location, owner){
+        this.location = location
+        this.owner = owner
+        this.vehicles = []
+    }
+
+    //create Vehicle
+    createVehicle(vehicle){
+        this.vehicle.push(vehicle)
+    }
+    //paints the matching vehicle the specified color
+    paintVehicle(vin, paintColor){
+        //index of the vehicle with that vin
+        let vIndex = this.vehicles.findIndex( v => v.vin === vin)
+        this.vehicles[vIndex].paint(paintColor)
+    }
+    //how to implement list vehicles
+    listVehicles(){
+        return this.vehicles
+    }
 }
