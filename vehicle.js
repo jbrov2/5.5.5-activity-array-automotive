552c859307f749034edec4bf33a7fe5bba5d2767
zephyr <jafaribrownsha@gmail.com>
2023-09-20 22:02:21 -0400

    added constructor and classes to pass classes.

--- vehicle.js
+++ vehicle.js
@@ -1,4 +1,19 @@
-class Vehicle {
-    
-}
+class Vehicle{
+    constructor(make, model, year ,price, vin){
+        this.make = make
+        this.model = model
+        this.year = year
+        this.price = price
+        this.vin = vin
+        this.color = paint()
+    }
 
+    //paint method
+    paint(color){
+        this.color = color
+    }
+
+    drive(){
+        return this.year +' '+ this.make + ' '+ this.model + ' drives'
+    }
+}
